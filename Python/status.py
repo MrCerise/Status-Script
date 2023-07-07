2fea825acf0a80c65961f9796a667e5ccfffb52f
Mr CeriseYTB <103898291+MrCerise@users.noreply.github.com>
2023-07-07 14:21:07 +0200

    Update status.py
    Adding online, offline, idle and dnd discord status

--- Python/status.py
+++ Python/status.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import discord
 
 token = "User Token"
 status = True
@@ -9,23 +10,26 @@ status_list = [
     "text": "Text 1",
     "emoji_id": "ID",
     "emoji_name": "Name", # :css:
-    "interval": 3000
+    "interval": 3000, # Time in ms
+    "status": discord.Status.dnd # online,idle,offline,dnd
   },
   {
     "text": "Text 2",
     "emoji_id": None,
     "emoji_name": None,
-    "interval": 5000
+    "interval": 5000,
+    "status": discord.Status.idle
   },
   {
     "text": "Text 3",
     "emoji_id": None,
     "emoji_name": None,
-    "interval": 7000
+    "interval": 7000,
+    "status": discord.Status.online
   }
 ]
 
-def update_status(text, emoji_id, emoji_name):
+def update_status(text, emoji_id, emoji_name, status):
   try:
     url = "https://discord.com/api/v10/users/@me/settings"
     headers = {"Authorization": token}
@@ -34,7 +38,8 @@ def update_status(text, emoji_id, emoji_name):
         "text": text,
         "emoji_id": emoji_id,
         "emoji_name": emoji_name
-      }
+      },
+      "status": str(status)
     }
     response = requests.patch(url, json=payload, headers=headers)
     print("Updated")
@@ -43,17 +48,15 @@ def update_status(text, emoji_id, emoji_name):
     print(f"Failed to update status. Error: {error}")
     return False
 
-def main():
+def main(status):
   while status:
     for status_item in status_list:
       text = status_item["text"]
       emoji_id = status_item["emoji_id"]
       emoji_name = status_item["emoji_name"]
       interval = status_item["interval"]
-      updated = update_status(text, emoji_id, emoji_name)
-      if updated:
-        time.sleep(interval / 1000)
-      else:
-        break
+      status = status_item["status"]
+      updated = update_status(text, emoji_id, emoji_name, status)
+      time.sleep(interval / 1000)
 
-main()
+main(status)
